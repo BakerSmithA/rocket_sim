bd0ddd0333c10528d9dd968e7d1ce48c9a591992
Tomos Evans <tomosamevans@gmail.com>
2019-02-16 11:39:35 +0000

    added simulation object

--- rocket_sim/simulations.py
+++ rocket_sim/simulations.py
@@ -1,9 +1,56 @@
-from typing import List
+from typing import List,Tuple
 from .vehicle import Vehicle
 from .state import VehicleState
+from .calculations import apogee, maximum_acceleration, g_force
+from .graphics import time_series_plot_group
 
 
-def simulate(vehicle: Vehicle, dt: float) -> List[VehicleState]:
+class Simulation:
+
+    def __init__(self, states: List[VehicleState]):
+        self.states = states
+
+    @property
+    def events(self):
+        return [(s.time_s, s.event) for s in self.states if s.event is not None]
+
+    @property
+    def impact_velocity(self) -> float:
+        return self.states[-1].velocity_ms
+
+    @property
+    def maximum_acceleration(self) -> Tuple[float, float]:
+        return maximum_acceleration(self.states)
+
+    @property
+    def apogee(self) -> Tuple[float, float]:
+        return apogee(self.states)
+
+    @property
+    def total_time(self) -> float:
+        return self.states[-1].time_s
+
+    @property
+    def time_series(self) -> List[float]:
+        return [s.time_s for s in self.states]
+
+    @property
+    def maximum_g_force(self):
+        max_acceleration, max_acceleration_time = self.maximum_acceleration
+        return g_force(max_acceleration), max_acceleration_time
+
+    def display_plots(self, title) -> None:
+        time_series_plot_group(title, [
+            (self.time_series, [s.mass_kg for s in self.states], self.events, 'Time (s)', 'Mass (kg)'),
+            (self.time_series, [s.accel_ms2 for s in self.states], self.events, 'Time (s)', 'Acceleration (m/s2)'),
+            (self.time_series, [s.velocity_ms for s in self.states], self.events, 'Time (s)', 'Velocity (m/s)'),
+            (self.time_series, [s.dist_m for s in self.states], self.events, 'Time (s)', 'Altitude (m)'),
+            (self.time_series, [s.thrust_N for s in self.states], self.events, 'Time (s)', 'Thrust (N)'),
+            (self.time_series, [s.air_resistance_N for s in self.states], self.events, 'Time(s)', 'Air Resistance (N)'),
+        ])
+
+
+def simulate(vehicle: Vehicle, dt: float) -> Simulation:
     """
     :param vehicle: vehicle to simulate.
     :param dt: time step, i.e. resolution.
@@ -22,6 +69,12 @@ def simulate(vehicle: Vehicle, dt: float) -> List[VehicleState]:
         vehicle = vehicle.step(dt)
         states.append(vehicle.state)
 
-    return states
+    apogee_m, apogee_time_s = apogee(states)
+
+    for state in states:
+        if state.dist_m == apogee_m and state.time_s == apogee_time_s:
+            state.event = 'Apogee'
+
+    return Simulation(states)
 
 
--- simulation.py
+++ simulation.py
@@ -1,35 +1,20 @@
 from rocket_sim.simulations import simulate
-from rocket_sim.calculations import apogee, maximum_acceleration, g_force
-from rocket_sim.graphics import time_series_plot_group
 from vehicle_examples import single_stage, three_stage
 
 vehicle, vehicle_name = three_stage()
 
-states = simulate(vehicle, dt=0.1)
+simulation = simulate(vehicle, dt=0.1)
 
-apogee_m, apogee_time_s = apogee(states)
-impact_velocity_ms = states[-1].velocity_ms
-max_accel_ms2, max_accel_time_s = maximum_acceleration(states)
-total_time = states[-1].time_s
-
-time = [s.time_s for s in states]
-events = [(s.time_s, s.event) for s in states if s.event is not None]
-events.append((apogee_time_s, 'Apogee'))
+apogee_m, apogee_time_s = simulation.apogee
+max_g_force, max_g_force_time_s = simulation.maximum_g_force
 
 print(f'Apogee           (m):   {apogee_m}')
 print(f'Time to apogee   (s):   {apogee_time_s}')
-print(f'Time to land     (s):   {total_time - apogee_time_s}')
-print(f'Time             (s):   {total_time}')
-print(f'Impact Velocity  (m/s): {impact_velocity_ms}')
-print(f'Max G-force:            {g_force(max_accel_ms2)}')
-print(f'Max G-force time (s):   {max_accel_time_s}')
+print(f'Time to land     (s):   {simulation.total_time - apogee_time_s}')
+print(f'Time             (s):   {simulation.total_time}')
+print(f'Impact Velocity  (m/s): {simulation.impact_velocity}')
+print(f'Max G-force:            {max_g_force}')
+print(f'Max G-force time (s):   {max_g_force_time_s}')
 
-time_series_plot_group(vehicle_name, [
-    (time, [s.mass_kg for s in states], events, 'Time (s)', 'Mass (kg)'),
-    (time, [s.accel_ms2 for s in states], events, 'Time (s)', 'Acceleration (m/s2)'),
-    (time, [s.velocity_ms for s in states], events, 'Time (s)', 'Velocity (m/s)'),
-    (time, [s.dist_m for s in states], events, 'Time (s)', 'Altitude (m)'),
-    (time, [s.thrust_N for s in states], events, 'Time (s)', 'Thrust (N)'),
-    (time, [s.air_resistance_N for s in states], events, 'Time (s)', 'Air Resistance (N)'),
-    ]
-)
+
+simulation.display_plots(vehicle_name)
